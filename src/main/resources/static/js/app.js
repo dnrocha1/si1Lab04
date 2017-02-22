e6a4f7d5da7ca9e5762b7b39485aba4c32d9f9b5
Daniyel <daniyel.rocha@ccc.ufcg.edu.br>
2017-02-22 18:53:08 -0300

    Criação da tela de página principal e configuração da rota.

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,30 +2,41 @@ var app = angular.module('todoApp', ['ui.router']);
 
 
 //ui.router
-app.config(function ($stateProvider) {
+app.config(function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise( function($injector) {
+        var $state = $injector.get("$state");
+        $state.go('home');
+    });
+
     $stateProvider
-        .state('todoList',{
-            url:'/todoList',
+        .state('todoList', {
+            url: '/todoList',
             templateUrl: 'view/todoList.html',
             controller: 'todoListController'
         })
-        .state('task',{
-            url:'/tasks/:todoListId',
+        .state('task', {
+            url: '/tasks/:todoListId',
             templateUrl: 'view/task.html',
             controller: 'taskController'
         })
-        .state('contact',{
-            url:'/contact',
+        .state('contact', {
+            url: '/contact',
             templateUrl: 'view/contact.html'
         })
-        .state('allTasks',{
-            url:'/allTasks',
+        .state('allTasks', {
+            url: '/allTasks',
             templateUrl: 'view/allTasks.html',
             controller: 'allTasksController'
         })
-        .state('category',{
-            url:'/taskcategories',
+        .state('category', {
+            url: '/taskcategories',
             templateUrl: 'view/category.html',
             controller: 'categoryController'
+        })
+        .state('home', {
+            url: 'home',
+            templateUrl: 'view/home.html',
+            controller: 'homeController'
         })
 });
---
+++ src/main/resources/static/js/controller/home-controller.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Daniyel on 22/02/2017.
+ */
+angular.module('todoApp').controller("homeController", function ($scope, $http) {
+
+});
