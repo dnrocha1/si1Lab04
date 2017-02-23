da7a97611653eeac07ff5fbc440a7933ac64a9fa
dnrocha1 <daniyel.rocha@ccc.ufcg.edu.br>
2017-02-23 13:12:43 -0300

    Alteração no pom e no main.

--- src/main/java/com/daniyel/si1lab04/Si1lab04Application.java
+++ src/main/java/com/daniyel/si1lab04/Si1lab04Application.java
@@ -2,11 +2,28 @@ package com.daniyel.si1lab04;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Si1lab04Application {
 
+	private static final String[] REQUEST_METHOD_SUPPORTED = { "GET", "POST", "PUT", "DELETE" };
+
 	public static void main(String[] args) {
 		SpringApplication.run(Si1lab04Application.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*")
+						.allowedMethods(REQUEST_METHOD_SUPPORTED);
+			}
+		};
 	}
 }
